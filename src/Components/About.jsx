aebc89b2fc32c35e6c9aca40aa4c1a7e952585c9
Saloni Pandey <salonipandey014@gmail.com>
2023-06-12 14:26:28 +0530

    Implemented Class Based Component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,27 +1,41 @@
 import Profile from "./ProfileClass"
 import React from "react";
-// const About = ()=>{
-//     return <>
-//     <h1>This is about me </h1>
-//     <Profile name = "Saloni Pandey"/>
-//     </>
-// }
-class About extends React.Component{
-    constructor(props){
-        super(props)
-        console.log("hello1")
+import {useEffect} from "react"
+const About = ()=>{
+
+   useEffect(()=>{
+    const interval = setInterval(()=>{
+        console.log("Component mount");
+    },1000)
+    return ()=>{
+        clearInterval(interval);
+        console.log("Component unmount")
     }
-    componentDidMount(){
-        console.log("Hello3")
-    }
-    
-    render(){
-        console.log("Hello2")
-        return <div className="about">
-            <h1>This About me</h1>
-            <Profile name = "Saloni Pandey"/>
-        </div>
-    }
+   })
+
+    return <>
+    <h1>This is about me </h1>
+    <Profile name = "Saloni Pandey"/>
+    </>
 }
+// class About extends React.Component{
+//     constructor(props){
+//         super(props)
+//         console.log("hello1")
+//     }
+//     // async componentDidMount(){
+//     //     let data = await fetch("https://api.github.com/users/Saloni-FED");
+//     //     console.log(data);
+//     //     }
+    
+    
+//     render(){
+//         console.log("Hello2")
+//         return <div className="about">
+//             <h1>This About me</h1>
+//             <Profile name = "Saloni Pandey"/>
+//         </div>
+//     }
+// }
 
 export default About;
