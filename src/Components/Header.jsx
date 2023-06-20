1fde63c57880d4810f82084ca8a8fec109b83481
Saloni Pandey <salonipandey014@gmail.com>
2023-06-20 20:20:16 +0530

    Fixed issue in Search bar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,24 +12,24 @@ const HeaderComponent = () => {
       <div className="flex bg-black text-lg text-white justify-between shadow-xl border-box" >
         <img className="w-44 h-20 ml-40 py-2" src="./public/Food logo.png" />
         <ul className="flex mt-5 justify-around pr-50">
-          <li className="mr-6">
+          <li className="mr-6 hover:font-bold">
             <Link to="/">Home</Link>
           </li>
-          <li className="mr-6">
+          <li className="mr-6 hover:font-bold">
             <Link to="/about">About</Link>
           </li>
-          <li className="mr-6">
+          <li className="mr-6 hover:font-bold">
             <Link to="/contact">Contact</Link>
           </li>
-          <li className="mr-6">
+          <li className="mr-6 hover:font-bold">
             <img
-              className="w-10"
+              className="w-10 hover:font-bold"
               src="https://www.shutterstock.com/image-illustration/brown-map-pointer-icon-shopping-600w-272565896.jpg"
               alt="cart"
               srcset=""
             />
           </li>
-          <li>
+          <li className="hover:font-bold">
             <Link to="/instamart">Mart</Link>
           </li>
         </ul>
